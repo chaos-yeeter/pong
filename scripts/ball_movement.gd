b01fc25714ad7e7ff0c057f388891ffc5a9d8928
chaos-yeeter <kayasthamihir11@gmail.com>
2023-11-29 23:38:57 +0530

    feat: Add ball that bounces on collision

---
+++ scripts/ball_movement.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+@export var movement_speed: float = 200
+
+func _ready():
+	velocity = Vector2(randf_range(0.1, 0.7), randf_range(0.1, 0.7)) * movement_speed
+
+	
+func _physics_process(delta):
+	var collision_info = move_and_collide(velocity * delta)
+	if collision_info:
+		velocity = velocity.bounce(collision_info.get_normal())
