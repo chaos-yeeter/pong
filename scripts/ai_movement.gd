36d20b530866fff9ee5ede2089b7f9f5e5f3dea5
chaos-yeeter <kayasthamihir11@gmail.com>
2023-11-30 18:58:28 +0530

    feat(ai): Make ai paddle follow the ball

--- scripts/ai_movement.gd
+++ scripts/ai_movement.gd
@@ -1,11 +1,16 @@
 extends CharacterBody2D
 
 @export var movement_speed: float = 100
+@export var acceleration: float = 1.5
+@export var ball: CharacterBody2D
 
-var speed: Vector2 = Vector2(0, movement_speed)
 
 func _process(delta):
-	if get_slide_collision_count() > 0:
-		speed = speed * -1
-	velocity = speed
+	var current_y_position = self.get_position().y
+	var ball_y_position = ball.get_position().y
+	var movement_direction = Vector2(
+		0,
+		(ball_y_position - current_y_position) / abs(ball_y_position - current_y_position)
+	)
+	velocity = velocity.move_toward(movement_direction * movement_speed, acceleration)
 	move_and_slide()
