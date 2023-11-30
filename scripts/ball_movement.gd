86710e58669632c580b95c7fd82f1ff290900dad
chaos-yeeter <kayasthamihir11@gmail.com>
2023-11-30 19:18:03 +0530

    ref(ball): Reduce movement speed and change range for initial angle
    - Reduce movement_speed 250 -> 150
    - Make it so that the ball does not go too vertical or too horizontal

--- scripts/ball_movement.gd
+++ scripts/ball_movement.gd
@@ -1,10 +1,9 @@
 extends CharacterBody2D
 
-@export var movement_speed: float = 250
+@export var movement_speed: float = 200
 
 func _ready():
-	velocity = Vector2(randf_range(0.1, 0.7), randf_range(0.1, 0.7)) * movement_speed
-
+	velocity = Vector2(randf_range(0.7, 0.8), randf_range(-0.7, 0.7)) * movement_speed
 	
 func _physics_process(delta):
 	var collision_info = move_and_collide(velocity * delta)
