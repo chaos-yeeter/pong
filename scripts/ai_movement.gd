f61997f3e1e1d7c75d9dc18f1a5890266639e237
chaos-yeeter <kayasthamihir11@gmail.com>
2023-11-30 19:08:58 +0530

    fix(ai): Run ai movement computation once every 10 frames
    - Add acceleration for smooth movement

--- scripts/ai_movement.gd
+++ scripts/ai_movement.gd
@@ -1,16 +1,19 @@
 extends CharacterBody2D
 
 @export var movement_speed: float = 100
-@export var acceleration: float = 1.5
+@export var acceleration: float = 32
 @export var ball: CharacterBody2D
 
 
-func _process(delta):
-	var current_y_position = self.get_position().y
-	var ball_y_position = ball.get_position().y
-	var movement_direction = Vector2(
-		0,
-		(ball_y_position - current_y_position) / abs(ball_y_position - current_y_position)
-	)
-	velocity = velocity.move_toward(movement_direction * movement_speed, acceleration)
+func _process(_delta):
+	# run computation once every x frames
+	if Engine.get_frames_drawn() % 10 == 0:
+		var current_y_position = self.get_position().y
+		var ball_y_position = ball.get_position().y
+		# get unit vector in the direction of ball
+		var movement_direction = Vector2(
+			0,
+			(ball_y_position - current_y_position) / abs(ball_y_position - current_y_position)
+		)
+		velocity = velocity.move_toward(movement_direction * movement_speed, acceleration)
 	move_and_slide()
