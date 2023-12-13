c13c4f04d7a5c3174bb9da3cca961ad7702952be
chaos-yeeter <kayasthamihir11@gmail.com>
2023-12-13 07:48:42 +0530

    ref(player): Add validation to check if signal exists

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -6,12 +6,14 @@ const DECELERATION: float = 10
 var _initial_position: Vector2
 
 func _ready():
+	# validations
+	assert(Signals.has_signal("ball_hit_death_zone"), "Signal 'ball_hit_death_zone' not found")
+	
+	# initialization
 	_initial_position = self.get_position()
+	
+	# signals
 	Signals.ball_hit_death_zone.connect(_reset)
-
-func _reset(_body):
-	velocity = Vector2.ZERO
-	self.set_position(_initial_position)
 
 func _process(_delta):
 	velocity = velocity.move_toward(Vector2.ZERO, DECELERATION)
@@ -20,3 +22,7 @@ func _process(_delta):
 	elif Input.is_action_pressed("move_down"):
 		velocity = Vector2.DOWN * MOVEMENT_SPEED
 	move_and_slide()
+
+func _reset(_body):
+	velocity = Vector2.ZERO
+	self.set_position(_initial_position)
